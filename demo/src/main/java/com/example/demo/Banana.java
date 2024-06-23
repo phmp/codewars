67dbdf47c011e266db5fa54e0014430c900c38e9
Pawel Proc <pawel.henryk.proc@gmail.com>
2024-06-23 03:01:00 +0200

    refactor

--- demo/src/main/java/com/example/demo/Banana.java
+++ demo/src/main/java/com/example/demo/Banana.java
@@ -9,55 +9,50 @@ public class Banana {
 
 
     static Set<String> bananas(final String s) {
-        List<Character> input = s.chars().mapToObj(a -> (char) a).toList();
-
-        return simpleBanana(input, 0, 0);
+        return simpleBanana(s, 0, 0);
     }
 
-    private static Set<String> simpleBanana(List<Character> input, int startIndex, int startBananaIndex) {
-        System.out.println("input: " + input);
-        System.out.println("startIndex: " + startIndex);
-        System.out.println("startBananaIndex: " + startBananaIndex);
-
+    private static Set<String> simpleBanana(String current, int startIndex, int startBananaIndex) {
         Set<String> result = new HashSet<>();
-        List<Character> current = new ArrayList<>(input);
-        for (int currentIndex = startIndex, banannasIndex = startBananaIndex; currentIndex < current.size(); currentIndex++) {
+        for (int currentIndex = startIndex, banannasIndex = startBananaIndex; currentIndex < current.length(); currentIndex++) {
             if(banannasIndex >= BANANAS.length()){
-                current.set(currentIndex, '-');
+                current = crossLetter(current, currentIndex);
                 continue;
             }
 
-            Character letter = current.get(currentIndex);
+            Character letter = current.charAt(currentIndex);
             if (isNotBorNorA(letter)) {
-                current.set(currentIndex, '-');
+                current = crossLetter(current, currentIndex);
                 continue;
             }
 
             if (letter.equals(BANANAS.charAt(banannasIndex))){
                 Set<String> withTheSameLetter = simpleBanana(current, currentIndex+1, banannasIndex+1);
-                current.set(currentIndex, '-');
-                Set<String> withCrossedLetter = simpleBanana(current, currentIndex+1, banannasIndex);
-
+                Set<String> withCrossedLetter = simpleBanana(crossLetter(current, currentIndex), currentIndex+1, banannasIndex);
                 return mergeSet(withTheSameLetter, withCrossedLetter);
             } else {
-                current.set(currentIndex, '-');
+                current = crossLetter(current, currentIndex);
             }
 
         }
 
         if(withoutDashesIsBanana(current)) {
-            result.add(current.stream().map(String::valueOf).collect(Collectors.joining()));
+            result.add(current);
         }
 
         return result;
+    }
+
+    private static String crossLetter(String word, int letterIndex) {
+        return word.substring(0, letterIndex) + "-" + word.substring(letterIndex+1);
     }
 
     private static boolean isNotBorNorA(Character letter) {
         return BANANAS.indexOf(letter) == -1;
     }
 
-    private static boolean withoutDashesIsBanana(List<Character> current) {
-        return current.stream().filter(a -> a != '-').map(String::valueOf).collect(Collectors.joining()).equals(BANANAS);
+    private static boolean withoutDashesIsBanana(String current) {
+        return current.replaceAll("-","").equals(BANANAS);
     }
 
     public static Set<String> mergeSet(Set<String> a, Set<String> b)
